e91ca93a243e3fe0745a1e788cc0f2132d5cd9e3
ylim-mm <73403661+ylim-mm@users.noreply.github.com>
2026-08-12 07:20:55 -0400

    Update missplot.R

--- missplot.R
+++ missplot.R
@@ -2,50 +2,47 @@
 library(dplyr)
 library(naniar)
 
-# Read the raw data file
-data <- read.csv( "raw_data.csv")
+# Read the raw data
+data <- read.csv( "final_data.csv")
 
-# Extract the age of interest
+# Extract the age
 df <- subset(data, data$age >=9 & data$age <=19.99)
-
 df$sex[df$sex == ""] <- NA  
-
-# numerical code 
 df$sex <- ifelse(df$sex == "M", 1, ifelse(df$sex == "F", 2, NA))
 
-#select columns w/ missing data
-missing_data <- df[, c(3:4, 28, 7, 9, 12:27)]
+# select columns w/ missing data
+missing_data <- df[, c(4, 28, 7, 6, 8, 11:26)]
 
-#convert everything to numeric 
+# convert everything to numeric 
 missing_data[] <- lapply(missing_data, function(x) {
   x[x == ""] <- NA
   as.numeric(x)
 })
 
-#rename columns for plot
+# rename columns for plot
 colnames(missing_data) <- c(
-  "BMI Percentile", "Household Income", "Pubertal Stage", 
-  "Age","Sex",
-  "SDSC Disorders of Arousal T-score", 
-  "SDSC Disorders of Initiating and Maintaining Sleep T-score", 
-  "SDSC Excessive Somnolence T-score",
-  "SDSC Sleep Breathing Disorder T-score", 
-  "SDSC Sleep Hyperhydrosis T-score", 
-  "SDSC Sleep Wake-Transition Disorders T-score", 
-  "SDSC Total T-score",
+  "Household Income", "Pubertal Stage", "BMI", "Age","Sex", 
+  "SDSC Disorders of Arousal Score", 
+  "SDSC Disorders of Initiating and Maintaining Sleep Score", 
+  "SDSC Excessive Somnolence Score",
+  "SDSC Sleep Breathing Disorder Score", 
+  "SDSC Sleep Hyperhydrosis Score", 
+  "SDSC Sleep Wake-Transition Disorders Score", 
+  "SDSC Total Score",
   "SCARED Generalized Anxiety Score", "SCARED Somatic/Panic Score", 
   "SCARED Social Anxiety Score", 
   "SCARED School Avoidance Score",
   "SCARED Separation Anxiety Score", 
   "SCARED Total Score",
-  "CBCL Internalizing T-Score", 
-  "CBCL Externalizing T-score", 
+  "CBCL Internalizing Score", 
+  "CBCL Externalizing Score", 
   "CBCL Total Score"
 )
 
-#create plot
+# create missing plot
 gg_miss_upset(
   missing_data,
-  nsets = ncol(missing_data),   # ensure all columns are included 
-  order.by = "freq"             # order by frequency 
+  nsets = ncol(missing_data),   
+  order.by = "freq"            
 )
+

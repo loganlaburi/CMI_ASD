ab473f570fbe7d7ff9f232c666fb2107f9f5659b
ylim-mm <73403661+ylim-mm@users.noreply.github.com>
2026-08-12 07:31:27 -0400

    Update preprocess.R

--- preprocess.R
+++ preprocess.R
@@ -3,138 +3,136 @@
 library(sjPlot)
 library(dplyr)
 library(broom)
-library(mice) 
+library(mice)
 
 # Read data
-asd_data <- read.csv('data_asd.csv')
-diag <- read.csv('diagnosis.csv')
-sleep <- read.csv('sleep_hr.csv')
-diag <- diag[, c("Identifiers", "Has_Autism")]
-cbcl <- read.csv('cbcl.csv')
+asd_data <- read.csv('asd_only.csv')
 
-# Attach the "has_autism" info based on dplyr
-data <- asd_data %>%
-  left_join(diag, by="Identifiers")
-
-raw_data <- subset(data, data$Has_Autism==1)
-
-# Combine all files together
-merged_data <- raw_data %>%
-  left_join(cbcl, by="Identifiers")
-
-df <- merged_data[, c("Identifiers", "bmi_percentile", "FSQ.FSQ_04",
-               "PPS.PPS_F_Score", "PPS.PPS_M_Score", 
-               "Physical.Age", "Physical.BMI", "Physical.Sex",
-               "PreInt_Demos_Fam.Child_Ethnicity",
-               "PreInt_Demos_Fam.Child_Race",
-               "SDS.SDS_DA_T", "SDS.SDS_DIMS_T",
-               "SDS.SDS_DOES_T", "SDS.SDS_SBD_T",
-               "SDS.SDS_SHY_T", "SDS.SDS_SWDT_T", "SDS.SDS_Total_T",
-               "SCARED_P.SCARED_P_GD", "SCARED_P.SCARED_P_PN",
-               "SCARED_P.SCARED_P_SC", "SCARED_P.SCARED_P_SH",
-               "SCARED_P.SCARED_P_SP", "SCARED_P.SCARED_P_Total",
-               "CBCL.CBCL_Int_T", "CBCL.CBCL_Ext_T", 
-               "CBCL.CBCL_Total_T")]
+# Extract only the followings
+df <- asd_data[, c("Identifiers", "bmi_percentile", "FSQ.FSQ_04", "SDS.SDS_01",
+                      "PPS.PPS_F_Score", "PPS.PPS_M_Score", 
+                      "Physical.Age", "Physical.BMI", "Physical.Sex",
+                      "PreInt_Demos_Fam.Child_Ethnicity",
+                      "PreInt_Demos_Fam.Child_Race",
+                      "SDS.SDS_DA_Raw", "SDS.SDS_DIMS_Raw",
+                      "SDS.SDS_DOES_Raw", "SDS.SDS_SBD_Raw",
+                      "SDS.SDS_SHY_Raw", "SDS.SDS_SWTD_Raw", "SDS.SDS_Total_Raw",
+                      "SCARED_P.SCARED_P_GD", "SCARED_P.SCARED_P_PN",
+                      "SCARED_P.SCARED_P_SC", "SCARED_P.SCARED_P_SH",
+                      "SCARED_P.SCARED_P_SP", "SCARED_P.SCARED_P_Total",
+                      "CBCL.CBCL_Int", "CBCL.CBCL_Ext", 
+                      "CBCL.CBCL_Total", "comorbidity_count")]
 
 # Change the column names
-colnames(df) <- c("Identifiers", "BMI_perc", "hhi",
+colnames(df) <- c("Identifiers", "bmi_percentile", "hhi", "sleep_hr",
                   "pps_f_total", "pps_m_total", "age", "BMI", "sex",
                   "ethnicity", "race", 
-                  "SDS_DA_T", "SDS_DIMS_T", "SDS_DOES_T",
-                  "SDS_SBD_T", "SDS_SHY_T", "SDS_SWDT_T", "SDS_Total_T",
+                  "SDS_DA", "SDS_DIMS", "SDS_DOES",
+                  "SDS_SBD", "SDS_SHY", "SDS_SWDT", "SDS_Total",
                   "SCARED_P_GD", "SCARED_P_PN", "SCARED_P_SC", 
                   "SCARED_P_SH","SCARED_P_SP", "SCARED_P_Total",
-                  "CBCL_Int_T", "CBCL_Ext_T", "CBCL_Total_T")
+                  "CBCL_Int", "CBCL_Ext", "CBCL_Total", "comorbidity_count")
+
+df <- df %>%
+  mutate(
+    pps_m_total = suppressWarnings(as.numeric(pps_m_total)),
+    pps_f_total = suppressWarnings(as.numeric(pps_f_total))
+  )
 
 # Put the puberty score into the categorical character, and convert 
 # back to the numeric 
 df <- df %>%
-  mutate(puberty_stage = case_when(
-    sex == "M" & pps_m_total == 3 ~ 1,                      # Prepubertal
-    sex == "M" & pps_m_total %in% c(4, 5) ~ 2,              # Beginning Pubertal
-    sex == "M" & pps_m_total %in% c(6, 7, 8) ~ 3,           # Mid-Pubertal
-    sex == "M" & pps_m_total %in% c(9, 10, 11) ~ 4,         # Advanced Pubertal
-    sex == "M" & pps_m_total == 12 ~ 5,
-    sex == "F" & pps_f_total == 1 ~ 1,                      # Prepubertal
-    sex == "F" & pps_f_total %in% 2 ~ 2,              # Beginning Pubertal
-    sex == "F" & pps_f_total %in% 3 ~ 3,           # Mid-Pubertal
-    sex == "F" & pps_f_total %in% 4 ~ 4,         # Advanced Pubertal
-    sex == "F" & pps_f_total == 5 ~ 5 # Postpubertal
-  ))
+  mutate(
+    puberty_stage = case_when(
+      sex == 1 & pps_m_total == 3 ~ 1,
+      sex == 1 & pps_m_total %in% c(4, 5) ~ 2,
+      sex == 1 & pps_m_total %in% c(6, 7, 8) ~ 3,
+      sex == 1 & pps_m_total %in% c(9, 10, 11) ~ 4,
+      sex == 1 & pps_m_total == 12 ~ 5,
+      
+      sex == 2 & pps_f_total == 1 ~ 1,
+      sex == 2 & pps_f_total %in% c(2) ~ 2,
+      sex == 2 & pps_f_total %in% c(3) ~ 3,
+      sex == 2 & pps_f_total %in% c(4) ~ 4,
+      sex == 2 & pps_f_total == 5 ~ 5,
+      
+      TRUE ~ NA_real_
+    )
+  )
 
 # convert everything except sex to numeric
 df <- df %>%
-  mutate(across(.cols = -c(sex, Identifiers), ~as.numeric(as.character(.x))))
+  dplyr::mutate(
+    across(-c("Identifiers"), ~as.numeric(as.character(.x)))
+  )
 
-# Add sleep information
-dfs <- left_join(df, sleep, by = "Identifiers")
-
-# sex coded M = 1 ; F = 2
-dfs <- dfs %>%
-  mutate(sex = recode(sex, "M" = 1, "F" = 2))
+# Remove the columns
+df <- df %>%
+  dplyr::select(-c(pps_f_total, pps_m_total))
 
 # Replace Unknown for NA 
-dfs$hhi <- replace(dfs$hhi, dfs$hhi==12, NA)
-dfs$sex <- replace(dfs$sex, dfs$sex==".", NA)
-dfs$race <- replace(dfs$race, dfs$race==10 | dfs$race == 11, NA)
-dfs$ethnicity <- replace(df$ethnicity, dfs$ethnicity==2 | dfs$ethnicity == 3, NA)
+df[df == "."] <- NA
+df$hhi <- replace(df$hhi, df$hhi==12, NA)
+df$sex <- replace(df$sex, df$sex==".", NA)
+df$race <- replace(df$race, df$race==10 | df$race == 11, NA)
+df$ethnicity <- replace(df$ethnicity, df$ethnicity==2 | df$ethnicity == 3, NA)
 
-# Save the raw data file
-write.csv(dfs, "raw_data.csv")
+write.csv(df, "final_data.csv")
 
-## IMPUTATION 
-# make the copy of raw data
-data <- dfs
-
+# Imputation
+data <- df
 dat_imp <- data
 
-colSums(is.na(dat_imp)) # check the number of missingness for each variable
+colSums(is.na(dat_imp)) 
 
-# Make sure what is needed to factorize is factorize
 dat_imp$sex     <- factor(dat_imp$sex)              
 dat_imp$hhi  <- if (!is.ordered(dat_imp$hhi)) ordered(dat_imp$hhi) else dat_imp$income
 dat_imp$puberty_stage <- if (!is.ordered(dat_imp$puberty_stage)) ordered(dat_imp$puberty_stage) else dat_imp$puberty_stage
-dat_imp$sleep_hr <- if (!is.ordered(dat_imp$sleep_hr)) ordered(dat_imp$sleep_hr) else dat_imp$sleep_hr
 dat_imp$race <- if (!is.ordered(dat_imp$race)) ordered(dat_imp$race) else dat_imp$race
 dat_imp$ethnicity <- if (!is.ordered(dat_imp$ethnicity)) ordered(dat_imp$ethnicity) else dat_imp$ethnicity
 
+# Define imputation variable
 vars_outcomes <- c(
-  "SDS_DA_T","SDS_DIMS_T","SDS_DOES_T","SDS_SBD_T", "SDS_SHY_T", "SDS_SWDT_T",
+  "SDS_DA","SDS_DIMS","SDS_DOES","SDS_SBD", "SDS_SHY", "SDS_SWDT", "SDS_Total",
   "SCARED_P_GD" ,"SCARED_P_PN","SCARED_P_SC", "SCARED_P_SH", "SCARED_P_SP","SCARED_P_Total",
-  "CBCL_Int_T","CBCL_Ext_T","CBCL_Total_T"       
+  "CBCL_Int","CBCL_Ext","CBCL_Total"       
 )
 
-vars_covars <- c("Identifiers", "BMI_perc", "hhi", "puberty_stage", "sleep_hr", "age",
+vars_covars <- c("Identifiers", "hhi", "puberty_stage", "age", "bmi_percentile", "sleep_hr",
                  "BMI", "sex", "ethnicity", "race", "comorbidity_count")
 
 vars_all    <- c(vars_outcomes, vars_covars)
 
-# Only extract the variable that will be imputed
+# Subset to working frame
 dat_imp <- dat_imp[, vars_all, drop = FALSE]
 
 ini  <- mice::mice(dat_imp, maxit = 0)
 meth <- ini$method
 pred <- ini$predictorMatrix
 
+# Methods for outcomes 
 meth[vars_outcomes] <- "pmm"
 
+# Methods for covariates
 meth["age"]      <- "pmm"
-meth["sex"]      <- "logreg"    # binary
+meth["sex"]      <- "logreg"    
+meth["bmi_percentile"]      <- "pmm"
 meth["hhi"]   <- if (is.ordered(dat_imp$income)) "polr" else "polyreg"
-meth["sleep_hr"]   <- if (is.ordered(dat_imp$sleep_hr)) "polr" else "polyreg"
 meth["puberty_stage"]  <- if (is.ordered(dat_imp$puberty_stage)) "polr" else "polyreg"
 meth["ethnicity"]  <- if (is.ordered(dat_imp$ethnicity)) "polr" else "polyreg"
 meth["race"]  <- if (is.ordered(dat_imp$race)) "polr" else "polyreg"
 meth["BMI"]      <- "pmm"
-meth["BMI_perc"] <- "pmm"  
 meth["comorbidity_count"] <- "pmm"
+meth["sleep_hr"] <- "pmm"
 
-# exclude ID for imputation (because we cannot impute the ID) 
+# Do NOT impute ID
 meth["Identifiers"] <- ""
+
+# Exclude subject_id as predictor & target
 pred[, "Identifiers"] <- 0
 pred["Identifiers", ] <- 0
 
-# Run mice (make sure the 'mice package' is loaded
+# Run MICE 
 imp <- mice::mice(
   dat_imp,
   m = 30,
@@ -144,6 +142,6 @@ imp <- mice::mice(
   seed = 20260108
 )
 
-# Extract the complete set and save it
+# Extract the completed set
 data_comp1 <- mice::complete(imp, action = "all")
 saveRDS(data_comp1, file = "data_after_mice.rds")
